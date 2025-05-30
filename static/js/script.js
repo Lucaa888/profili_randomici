9a65fafe4b0b8ef9ebc94aec7a97b90595b843da
Lucaa888 <illiano.luca.li@gmail.com>
2025-05-30 17:36:37 +0200

    fine

--- static/js/script.js
+++ static/js/script.js
@@ -1,20 +1,20 @@
 let url = "https://randomuser.me/api/";
-    let glbPerson;
+let glbPerson;
 
-    let n = document.querySelector(".name");
-    let e = document.querySelector(".email");
-    let p = document.querySelector(".phone");
-    let d = document.querySelector(".dob");
-    let l = document.querySelector(".location");
-    let c = document.querySelector(".country");
-    let img = document.querySelector(".picture");
+let n = document.querySelector(".name");
+let e = document.querySelector(".email");
+let p = document.querySelector(".phone");
+let d = document.querySelector(".dob");
+let l = document.querySelector(".location");
+let c = document.querySelector(".country");
+let img = document.querySelector(".picture");
 
-    fetch(url).then(
-        function (resp) {
+function fetchUser() {
+    fetch(url)
+        .then(function (resp) {
             return resp.json();
-        }
-    ).then(
-        function (data) {
+        })
+        .then(function (data) {
             glbPerson = data;
             img.setAttribute("src", glbPerson.results[0].picture.large);
             n.innerHTML = glbPerson.results[0].name.first + " " + glbPerson.results[0].name.last;
@@ -23,10 +23,14 @@ let url = "https://randomuser.me/api/";
             d.innerHTML = "Age: " + glbPerson.results[0].dob.age + " years";
             l.innerHTML = "Address: " + glbPerson.results[0].location.street.name + " " + glbPerson.results[0].location.street.number;
             c.innerHTML = "Country: " + glbPerson.results[0].location.state + ", " + glbPerson.results[0].location.country;
-        }
-    ).catch(
-        function (err) {
+        })
+        .catch(function (err) {
             console.log(err);
-        }
-    );
+        });
+}
 
+document.getElementById('generateUserBtn').addEventListener('click', function () {
+    fetchUser();
+});
+
+window.onload = fetchUser;
